59d385b4b842107d85b09f2a50a46b49c42ba66c
psunny0104 <psunny0104@gmail.com>
2019-09-04 01:20:40 +0900

    2557 / Hello World / 1st solution

---
+++ boj_2557.cpp
@@ -0,0 +1,16 @@
+//	@date 2019/09/04
+//	@author psunny0104
+//	@brief boj_2557_Hello World
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "Hello World!" << "\n";
+
+	return 0;
+}
