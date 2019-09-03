c9ff9d42e5d96c288cab56daf2eff8eda94c37ba
psunny0104 <psunny0104@gmail.com>
2019-09-04 01:28:39 +0900

    2558 / A+B - 2 / 1st solution

---
+++ boj_2558.cpp
@@ -0,0 +1,18 @@
+//	@date 2019/09/04
+//	@author psunny0104
+//	@brief boj_2558_A+B - 2
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int A = 0, B = 0;
+	cin >> A >> B;
+	cout << A + B << "\n";
+
+	return 0;
+}
