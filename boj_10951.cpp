db9cf916b4f465b9fefb68fefdbffa1c76d2f819
psunny0104 <psunny0104@gmail.com>
2019-09-04 01:35:35 +0900

    10951 / A+B - 4 / 1st solution

---
+++ boj_10951.cpp
@@ -0,0 +1,20 @@
+//	@date 2019/09/04
+//	@author psunny0104
+//	@brief boj_10951_A+B - 4
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int A = 0, B = 0;
+	//cin의 경우 입력받으면 True, 입력 받지 않으면 False를 return함을 이용
+	while (cin >> A >> B) { 
+		cout << A + B << "\n";
+	}
+
+	return 0;
+}
