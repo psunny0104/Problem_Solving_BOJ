064b4c36b707993be3b4cfd0b4ff1ec58fc26490
psunny0104 <psunny0104@gmail.com>
2019-09-10 17:50:37 +0900

    1001 / A-B / 1st solution

---
+++ boj_1001.cpp
@@ -0,0 +1,18 @@
+//	@date 2019/09/10
+//	@author psunny0104
+//	@brief boj_1001_A-B
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int A, B;
+	cin >> A >> B;
+	cout << A - B << "\n";
+
+	return 0;
+}
