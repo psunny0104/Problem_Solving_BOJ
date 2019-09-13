e2892ca6243d1bedbe6334760f397c371e1f6c9c
psunny0104 <53979482+psunny0104@users.noreply.github.com>
2019-09-13 22:11:17 +0900

    10998 / A×B / 1st solution

---
+++ boj_10998.cpp
@@ -0,0 +1,18 @@
+//	@date 2019/09/13
+//	@author psunny0104
+//	@brief boj_10998_A×B
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int A, B;
+	cin >> A >> B;
+	cout << A * B << "\n";
+
+	return 0;
+}
