8bd00a95abb901070aa90d46f87bdf38dddd5f70
psunny0104 <53979482+psunny0104@users.noreply.github.com>
2019-09-14 21:53:31 +0900

    8393 / 합 / 1st solution

---
+++ boj_8393.cpp
@@ -0,0 +1,19 @@
+//	@date 2019/09/14
+//	@author psunny0104
+//	@brief boj_8393_합
+ 
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+ 
+	int N;
+	cin >> N;
+
+	cout<<N*(N+1)/2<<"\n";
+ 
+	return 0;
+}
