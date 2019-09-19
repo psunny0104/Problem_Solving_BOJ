21477da93bd34387a108e3b5e8213fc2f6dbd6e2
psunny0104 <psunny0104@gmail.com>
2019-09-20 01:41:42 +0900

    1978 / 소수 찾기 / 1st solution

---
+++ boj_1978.cpp
@@ -0,0 +1,34 @@
+﻿//	@date 2019/09/04
+//	@author psunny0104
+//	@brief boj_1978_소수 찾기
+
+#include <iostream>
+using namespace std;
+
+int is_prime(int input)
+{
+	if (input < 2)
+		return 0;
+	for (int i = 2; i * i <= input; i++) {
+		if (input % i == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, num, ans = 0;
+	cin >> N;
+
+	while (N--) {
+		cin >> num;
+		ans += is_prime(num);
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
