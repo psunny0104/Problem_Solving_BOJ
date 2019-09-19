6f0bf9ad603fa6f501a55ec20ff3fa0059388f9a
psunny0104 <psunny0104@gmail.com>
2019-09-20 01:18:04 +0900

    10430 / 나머지 / 1st solution

---
+++ boj_10430.cpp
@@ -0,0 +1,23 @@
+﻿//	@date 2019/09/20
+//	@author psunny0104
+//	@brief boj_10430_나머지
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int A, B, C;
+
+	cin >> A >> B >> C;
+
+	cout << (A + B) % C << '\n';
+	cout << ((A % C) + (B % C)) % C << '\n';
+	cout << (A * B) % C << '\n';
+	cout << ((A % C) * (B % C)) % C << '\n';
+
+	return 0;
+}
