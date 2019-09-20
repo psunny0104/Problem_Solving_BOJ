8302d67d2a1fd7d211a1f1479a207d0317114031
psunny0104 <psunny0104@gmail.com>
2019-09-20 13:32:34 +0900

    1929 / 소수 구하기 / 1st solution

---
+++ boj_1929.cpp
@@ -0,0 +1,36 @@
+﻿//	@date 2019/09/04
+//	@author psunny0104
+//	@brief boj_1929_소수 구하기
+
+#include <iostream>
+#define MAX 1000001
+using namespace std;
+
+bool not_prime[MAX];
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int st, ed;
+	cin >> st >> ed;
+
+	not_prime[1] = true;
+	not_prime[2] = false;
+
+	for (int i = 2; i <= ed; i++) {
+		if (not_prime[i] == false) {
+			for (int j = i+i; j <= ed; j += i) {
+				not_prime[j] = true;
+			}
+		}
+	}
+
+	for (int i = st; i <= ed; i++) {
+		if (not_prime[i] == false)
+			cout << i << '\n';
+	}
+
+	return 0;
+}
