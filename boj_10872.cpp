83aa99fc0e0afbb0edf1bef17763107357be07ea
psunny0104 <psunny0104@gmail.com>
2019-09-20 14:14:58 +0900

    10872 / 팩토리얼 / 1st solution

---
+++ boj_10872.cpp
@@ -0,0 +1,26 @@
+﻿//	@date 2019/09/20
+//	@author psunny0104
+//	@brief boj_10872_팩토리얼
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	int ans = 1;
+	
+	while (N>=1) {
+		ans *= N;
+		N--;
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
