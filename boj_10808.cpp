d248353ede63d956b4579209d385d1f69a67d063
psunny0104 <psunny0104@gmail.com>
2019-09-21 16:15:50 +0900

    10808 / 알파벳 개수 / 1st solution

---
+++ boj_10808.cpp
@@ -0,0 +1,31 @@
+﻿//	@date 2019/09/21
+//	@author psunny0104
+//	@brief boj_10808 알파벳 개수
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	char word[101] = {'\0',};
+	int word_cnt[26] = { 0, };
+	int cnt = 0;
+
+	cin >> word;
+
+	while (true) {
+		if (word[cnt] == '\0')
+			break;
+		word_cnt[word[cnt] - 97]++;
+		cnt++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		cout << word_cnt[i] << ' ';
+	}
+
+	return 0;
+}
