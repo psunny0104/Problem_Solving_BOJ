4878992efa4116247e63ec8205e616f7d68a07a2
psunny0104 <psunny0104@gmail.com>
2019-09-22 02:06:09 +0900

    10809 / 알파벳 찾기 / 문자열 처리, 자료구조

---
+++ boj_10809.cpp
@@ -0,0 +1,32 @@
+﻿//	@date 2019/09/22
+//	@author psunny0104
+//	@brief boj_10809_알파벳 찾기
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int lc[26];
+	char word[101] = { '\0', };
+	memset(lc, -1, sizeof(lc));
+	
+	cin >> word;
+
+	for (int i = 0; i < 101; i++) {
+		if (word[i] == '\0')
+			break;
+		if(lc[word[i] -'a'] == -1)
+			lc[word[i] - 'a'] = i;
+	}
+	
+	for (int i = 0; i < 26; i++) {
+		cout << lc[i] << ' ';
+	}
+
+	return 0;
+}
