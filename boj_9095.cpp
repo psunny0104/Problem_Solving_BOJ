fe9147e4976eaca9423f9342f04b35221b7b75d4
psunny0104 <psunny0104@gmail.com>
2019-09-23 18:00:46 +0900

    9095 / 1, 2, 3 더하기 / dp, bottom-up

---
+++ boj_9095.cpp
@@ -0,0 +1,34 @@
+﻿//	@date 2019/09/22
+//	@author psunny0104
+//	@brief boj_9095_1, 2, 3 더하기
+
+#include <iostream>
+using namespace std;
+
+int memo[11];
+//memo[n] : 1, 2, 3의 합으로 나타내는 방법의 수
+//memo[n] : memo[n-1]+memo[n-2]+memo[n-3]]
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+
+	memo[1] = 1; //1도 가능
+	memo[2] = 2; //2도 가능
+	memo[3] = 4; //3도 가능
+
+	for (int i = 4; i <= 10; i++) {
+		memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+	}
+	while (T--) {
+		int N;
+		cin >> N;
+		cout << memo[N] << '\n';
+	}
+
+
+	return 0;
+}
