9c5cc9395e584afb80ad2650fe27fb28af597ee0
psunny0104 <psunny0104@gmail.com>
2019-09-24 15:41:36 +0900

    10988 / 팰린드롬인지 확인하기 / 문자열

---
+++ boj_10988.cpp
@@ -0,0 +1,27 @@
+﻿//	@date 2019/09/24
+//	@author psunny0104
+//	@brief boj_10988_팰린드롬인지 확인하기
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string word;
+	bool ans = true;
+	cin >> word;
+	for (int i = 0; i < word.size() / 2; i++) {
+		if (word[i] != word[word.size() - i - 1]) {
+			ans = false;
+			break;
+		}
+	}
+
+	if (ans == true ? cout << 1 << '\n' : cout << 0 << '\n');
+
+	return 0;
+}
