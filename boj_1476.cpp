797d2fc9e2edd630dfc794f474581d52c3612c21
psunny0104 <psunny0104@gmail.com>
2019-09-26 13:59:47 +0900

    1476 / 날짜 계산 / 브루트 포스, 수학

---
+++ boj_1476.cpp
@@ -0,0 +1,36 @@
+﻿//	@date 2019/09/26
+//	@author psunny0104
+//	@brief boj_1476_날짜 계산
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int E, S, M;
+	cin >> E >> S >> M;
+
+	int ans = 1;
+	while (E != 1 || S != 1 || M != 1) {
+		E--;
+		S--;
+		M--;
+
+		ans++;
+
+		if (E == 0)
+			E = 15;
+
+		if (S == 0)
+			S = 28;
+
+		if (M == 0)
+			M = 19;
+	}
+	cout << ans << '\n';
+
+	return 0;
+}
