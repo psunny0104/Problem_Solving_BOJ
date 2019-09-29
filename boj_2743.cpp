fb91aa0de9f379d41469328efcc636cd91c310a0
psunny0104 <psunny0104@gmail.com>
2019-09-29 23:46:19 +0900

    2743 / 단어 길이 재기 / 문자열

---
+++ boj_2743.cpp
@@ -0,0 +1,17 @@
+﻿//	@date 2019/09/29
+//	@author psunny0104
+//	@brief boj_2743_단어 길이 재기
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string word;
+	cin >> word;	
+	cout << word.size() << '\n';
+}
