74f569cc4fa70780579244758fbd62cfbba64d8f
psunny0104 <psunny0104@gmail.com>
2019-09-30 23:33:51 +0900

    10871 / X보다 작은 수 / 구현

---
+++ boj_10871.cpp
@@ -0,0 +1,21 @@
+﻿//	@date 2019/09/30
+//	@author psunny0104
+//	@brief boj_10871_X보다 작은 수
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, X, input;
+	cin >> N >> X;
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		if (input < X)
+			cout << input << ' ';
+	}
+	return 0;
+}
