5328c42eb32a28e1ac75d233cd9c44cefe401c0f
psunny0104 <psunny0104@gmail.com>
2019-09-30 23:40:15 +0900

    1110 / 더하기 사이클 / 수학

---
+++ boj_1110.cpp
@@ -0,0 +1,27 @@
+﻿//	@date 2019/09/30
+//	@author psunny0104
+//	@brief boj_1110_더하기 사이클
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N, OG, cnt = 0;
+	cin >>OG;
+	N = OG;
+	while (true) {
+		int first = (N % 10)* 10;
+		int second = (N / 10 + N % 10) % 10;
+		N = first + second;
+		cnt++;
+		if (N == OG)
+			break;
+	}
+	cout << cnt << '\n';
+
+	return 0;
+}
