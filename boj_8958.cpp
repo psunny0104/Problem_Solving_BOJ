6413931e491725c626238dfb9d97ff3fb46d724f
psunny0104 <psunny0104@gmail.com>
2019-10-02 23:44:30 +0900

    8958 / OX퀴즈 / 구현

---
+++ boj_8958.cpp
@@ -0,0 +1,34 @@
+﻿//	@date 2019/10/02
+//	@author psunny0104
+//	@brief boj_8958_OX퀴즈
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+	while (T--) {
+		string input;
+		cin >> input;
+		int scr = 0;
+		int cnt = 0;
+		for (int i = 0; i<input.size(); i++) {
+			if (input[i] == 'X') {
+				cnt = 0;
+			}
+			else {
+				cnt++;
+				scr += cnt;
+			}
+		}
+		cout << scr << '\n';
+	}
+
+	return 0;
+}
