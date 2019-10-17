36193e5897edc0eaec4b05aeda0e9b5c1e1a128f
psunny0104 <psunny0104@gmail.com>
2019-10-17 22:23:20 +0900

    2884 / 알람 시계 / 구현

---
+++ boj_2884.cpp
@@ -0,0 +1,27 @@
+﻿//	@date 2019/10/17
+//	@author psunny0104
+//	@brief boj_2884_알람 시계
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int H, M;
+	cin >> H >> M;
+
+	if (M >= 45) {
+		cout << H << ' ' << M - 45 << '\n';
+	}
+	else {
+		if (H == 0)
+			cout << 23 << ' ' << 60 + M - 45 << '\n';
+		else
+			cout << H - 1 << ' ' << 60 + M - 45 << '\n';
+	}
+
+	return 0;
+}
