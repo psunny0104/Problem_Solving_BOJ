6e5438ed76d1612cc6f9777f830556750d24ffe8
psunny0104 <psunny0104@gmail.com>
2019-10-21 00:06:36 +0900

    14889 / 스타트와 링크 / 브루트 포스

--- boj_14889.cpp
+++ boj_14889.cpp
@@ -11,63 +11,35 @@ int N, M, tg_cnt, tmp_cnt, ans;
 int rt[21][21];
 bool sltd[21];
 
-int cmb(int n, int k)
-{
-	if (k == n) 
-		return 1;
-	else if (k == 1) 
-		return n;
-	else 
-		return cmb(n - 1, k - 1) + cmb(n - 1, k);
-}
-
 void dfs(int idx, int cnt)
 {
 	if (cnt == M) {
-		tmp_cnt++;
-		vector<int> st;
-		vector<int> link;
-		for (int i = 1; i <= N; i++) {
-			if (sltd[i])
-				st.push_back(i);
-			else
-				link.push_back(i);
-		}
-
 		int scr_st = 0;
 		int scr_link = 0;
 
-		for (int i = 0; i < st.size(); i++) {
-			for (int j = 0; j < st.size(); j++) {
-				if (i == j)
-					continue;
-				scr_st += rt[st[i]][st[j]];
-			}
-		}
-		for (int i = 0; i < link.size(); i++) {
-			for (int j = 0; j < link.size(); j++) {
-				if (i == j)
-					continue;
-				scr_link += rt[link[i]][link[j]];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (sltd[i] && sltd[j])
+					scr_st += rt[i][j];
+				if (!sltd[i] && !sltd[j])
+					scr_link += rt[i][j];
 			}
 		}
 
-		ans = min(ans, abs(scr_st-scr_link));
+		ans = min(ans, abs(scr_st - scr_link));
 		return;
 	}
 
-	for (int i = idx; i <= N; i++) {
+	for (int i = idx; i < N; i++) {
 		if (sltd[i])
 			continue;
-		if (tmp_cnt == tg_cnt/2)
-			break;
 		sltd[i] = true;
 		dfs(i, cnt + 1);
 		sltd[i] = false;
 	}
 }
 
-int main() 
+int main()
 {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
@@ -82,7 +54,6 @@ int main()
 	M = N / 2;
 
 	ans = 987654321;
-	tg_cnt = cmb(N, M);
 	dfs(1, 0);
 	cout << ans << '\n';
 
