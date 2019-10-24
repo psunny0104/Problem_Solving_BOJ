cbfb3046271cb65164c4a2c8e81fdf47abae03da
psunny0104 <psunny0104@gmail.com>
2019-10-24 12:24:28 +0900

    2740 / 행렬 곱셈 / 구현

---
+++ boj_2740.cpp
@@ -0,0 +1,51 @@
+﻿//	@date 2019/10/24
+//	@author psunny0104
+//	@brief boj_2740_행렬 곱셈
+
+#include <iostream>
+using namespace std;
+
+int one[101][101];
+int two[101][101];
+int rt[101][101];
+int N, M, K;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			cin >> one[i][j];
+		}
+	}
+	cin >> M >> K;
+	for (int i = 1; i <= M; i++) {
+		for (int j = 1; j <= K; j++) {
+			cin >> two[i][j];
+		}
+	}
+	
+
+	for (int i = 1; i <= N; i++) {
+		for (int k = 1; k <= K; k++) {
+			int tmp = 0;
+			for (int m = 1; m <= M; m++) {
+				tmp += one[i][m] * two[m][k];
+			}
+			rt[i][k] = tmp;
+			tmp = 0;
+		}
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= K; j++) {
+			cout<<rt[i][j]<<' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
